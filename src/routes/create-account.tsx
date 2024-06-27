6675260e910ab91bcff947f834c39748cd8b8d23
hyojoo <jhj080802@naver.com>
2024-06-27 17:46:21 +0900

    #3.1 Forms and UI error(10:00)

--- src/routes/create-account.tsx
+++ src/routes/create-account.tsx
@@ -1,3 +1,96 @@
+import { useState } from "react";
+import styled from "styled-components";
+
+const Title = styled.h1`
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    width: 420px;
+    padding: 50px 0px;
+`;
+
+const Wrapper = styled.div`
+    font-size: 42px;
+`;
+
+const Form = styled.form`
+    margin-top: 50px;
+    display: flex;
+    flex-direction: column;
+    gap: 10px;
+    width: 100%;
+`;
+
+const Input = styled.input`
+    padding: 10px 20px;
+    border-radius: 50px;
+    border: none;
+    width: 100%;
+    font-size: 16px;
+`;
+
 export default function CreateAccount() {
-    return <h1>create account</h1>;
+    const [isLoading, setIsLoading] = useState(false);
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [error, setError] = useState("");
+
+    const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const {
+            target: { name, value },
+        } = e;
+        if (name === "name") {
+            setName(value);
+        } else if (name === email) {
+            setEmail(value);
+        } else if (name === password) {
+            setPassword(value);
+        }
+    };
+    const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        try {
+        } catch (e) {
+        } finally {
+            setIsLoading(false);
+        }
+    };
+    return (
+        <Wrapper>
+            <Title>Log into ✖️</Title>
+            <Form onSubmit={onSubmit}>
+                <Input
+                    onChange={onChange}
+                    name="name"
+                    value={name}
+                    placeholder="Name"
+                    type="text"
+                    required
+                />
+                <Input
+                    onChange={onChange}
+                    name="email"
+                    value={email}
+                    placeholder="Email"
+                    type="email"
+                    required
+                />
+                <Input
+                    onChange={onChange}
+                    name="password"
+                    value={password}
+                    placeholder="Password"
+                    type="password"
+                    required
+                />
+                <Input
+                    name="submit"
+                    value={isLoading ? "LaodingScreen..." : "Create Account"}
+                    value="Create Account"
+                />
+            </Form>
+        </Wrapper>
+    );
 }
