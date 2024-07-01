7157ad5baf72cc6cee95ba10422695fbbdf62d71
hyojoo <jhj080802@naver.com>
2024-07-01 20:16:29 +0900

    change styled

--- src/components/auth-components.ts
+++ src/components/auth-components.ts
@@ -10,16 +10,17 @@ export const Wrapper = styled.div`
 `;
 
 export const Title = styled.h1`
-  font-size: 42px;
+  font-size: 30px;
 `;
 
 export const Form = styled.form`
-  margin-top: 50px;
-  margin-bottom: 10px;
+  margin-top: 30px;
+  margin-bottom: 5px;
   display: flex;
   flex-direction: column;
   gap: 10px;
-  width: 100%;
+  width: 80%;
+  height: 60%;
 `;
 
 export const Input = styled.input`
@@ -27,7 +28,7 @@ export const Input = styled.input`
   border-radius: 50px;
   border: none;
   width: 100%;
-  font-size: 16px;
+  font-size: 13px;
   &[type='submit'] {
     cursor: pointer;
     &:hover {
