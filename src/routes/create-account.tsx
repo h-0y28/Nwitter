d77b2258f8b5f339d11483208e10d97dd12fd0cf
hyojoo <jhj080802@naver.com>
2024-07-01 18:33:49 +0900

    #3.4 Log In

--- src/routes/create-account.tsx
+++ src/routes/create-account.tsx
@@ -1,49 +1,13 @@
 import { createUserWithEmailAndPassword, updateProfile } from 'firebase/auth';
 import { useState } from 'react';
-import styled from 'styled-components';
 import { auth } from '../firebase';
-import { useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { FirebaseError } from 'firebase/app';
+import { Form, Error, Input, Switcher, Title, Wrapper } from '../components/auth-components';
 
-const Title = styled.h1`
-  height: 35%;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  width: 420px;
-  padding: 50px 0px;
-`;
-
-const Wrapper = styled.div`
-  font-size: 42px;
-`;
-
-const Form = styled.form`
-  margin-top: 50px;
-  display: flex;
-  flex-direction: column;
-  gap: 10px;
-  width: 100%;
-`;
-
-const Input = styled.input`
-  padding: 10px 20px;
-  border-radius: 50px;
-  border: none;
-  width: 100%;
-  font-size: 16px;
-  &[type='submit'] {
-    cursor: pointer;
-    &:hover {
-      opacity: 0.8;
-    }
-  }
-`;
-
-const Error = styled.span`
-  font-weight: 600;
-  color: red;
-`;
+// const errors = {
+//   "auth/email-already-in-use" :  "That email already exists."
+// }
 
 export default function CreateAccount() {
   const navigate = useNavigate();
@@ -68,6 +32,7 @@ export default function CreateAccount() {
 
   const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
+    setError('');
     if (isLoading || name === '' || email === '' || password === '') return;
     try {
       setIsLoading(true);
@@ -79,12 +44,13 @@ export default function CreateAccount() {
       navigate('/');
     } catch (e) {
       if (e instanceof FirebaseError) {
-        console.log(e.code, e.message);
+        setError(e.message);
       }
     } finally {
       setIsLoading(false);
     }
   };
+
   return (
     <Wrapper>
       <Title>Join 𝕏</Title>
@@ -95,6 +61,9 @@ export default function CreateAccount() {
         <Input type="submit" name="submit" value={isLoading ? 'LaodingScreen...' : 'Create Account'} />
       </Form>
       {error !== '' ? <Error>{error}</Error> : null}
+      <Switcher>
+        Already have an account? <Link to="/login">Log in &rarr;</Link>
+      </Switcher>
     </Wrapper>
   );
 }
