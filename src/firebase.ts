5011f75c4dfa79e18e9a86974f30a16d1e2349a7
hyojoo <jhj080802@naver.com>
2024-07-10 17:15:34 +0900

    code update

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,16 +1,22 @@
-import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
+import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDJ-bOc5PtF-fekzLmAuwMFnrNA2d4aFBM",
-  authDomain: "nwitter-reloaded-d362f.firebaseapp.com",
-  projectId: "nwitter-reloaded-d362f",
-  storageBucket: "nwitter-reloaded-d362f.appspot.com",
-  messagingSenderId: "497510104810",
-  appId: "1:497510104810:web:e892b68bacaa1af16d2193",
-  measurementId: "G-QNY8SCK0CV"
+  apiKey: 'AIzaSyDJ-bOc5PtF-fekzLmAuwMFnrNA2d4aFBM',
+  authDomain: 'nwitter-reloaded-d362f.firebaseapp.com',
+  projectId: 'nwitter-reloaded-d362f',
+  storageBucket: 'nwitter-reloaded-d362f.appspot.com',
+  messagingSenderId: '497510104810',
+  appId: '1:497510104810:web:e892b68bacaa1af16d2193',
+  measurementId: 'G-QNY8SCK0CV',
 };
 
 const app = initializeApp(firebaseConfig);
 
-export const auth= getAuth(app);
+export const auth = getAuth(app);
+
+export const storage = getStorage(app);
+
+export const db = getFirestore(app);
