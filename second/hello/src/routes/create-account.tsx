e87643d61476106effd612627b2d16f98d715e9f
hyojoo <jhj080802@naver.com>
2024-09-03 20:13:23 +0900

    create account

--- second/hello/src/routes/create-account.tsx
+++ second/hello/src/routes/create-account.tsx
@@ -1,3 +1,88 @@
+import React, { useState } from 'react';
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 420px;
+  padding: 50px 0px;
+`;
+
+const Title = styled.h1`
+  font-size: 42px;
+`;
+
+const Form = styled.form`
+  margin-top: 50px;
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+  width: 100%;
+`;
+
+const Input = styled.input`
+  padding: 10px 20px;
+  border-radius: 50px;
+  border: none;
+  width: 100%;
+  font-size: 16px;
+  &[type='submit'] {
+    cursor: pointer;
+    &:hover {
+      opacity: 0.8;
+    }
+  }
+`;
+
+const Error = styled.span`
+  font-weight: 600;
+  color: tomato;
+`;
+
 export default function CreateAccount() {
-  return <h1>Create acount</h1>;
+  const [isLoading, setIsLoading] = useState(false);
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+
+  // React.ChangeEvent<HTMLInputElement> : React에서 폼 요소의 onChange 이벤트를 처리할 때 사용되는 이벤트 객체의 타입을 정의하는 TypeScript 타입
+  // <input> 요소에서 발생하는 change 이벤트를 처리할 때 사용 됨
+
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const {
+      target: { name, value },
+    } = e;
+    if (name === 'name') {
+      setName(value);
+    } else if (name === 'email') {
+      setEmail(value);
+    } else if (name === 'password') {
+      setPassword(value);
+    }
+  };
+
+  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    try {
+    } catch (e) {
+    } finally {
+      setIsLoading(false);
+    }
+    console.log(name, email, password);
+  };
+  return (
+    <Wrapper>
+      <Title>Log into 𝕏</Title>
+      <Form onSubmit={onSubmit}>
+        <Input onChange={onChange} name="name" value={name} placeholder="Name" type="text" required />
+        <Input onChange={onChange} name="email" value={email} placeholder="Email" type="email" required />
+        <Input onChange={onChange} value={password} name="password" placeholder="Password" type="password" required />
+        <Input type="submit" value={isLoading ? 'Loading...' : 'Create Account'} />
+      </Form>
+      {error !== '' ? <Error>{error}</Error> : null}
+    </Wrapper>
+  );
 }
