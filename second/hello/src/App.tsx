91380be0449c06cb68a7a4fb8299769af178f46e
hyojoo <jhj080802@naver.com>
2024-09-03 19:34:48 +0900

    create grobal style and is loading

--- second/hello/src/App.tsx
+++ second/hello/src/App.tsx
@@ -4,6 +4,10 @@ import Home from './routes/home';
 import Profile from './routes/profile';
 import Login from './routes/login';
 import CreateAccount from './routes/create-account';
+import { createGlobalStyle } from 'styled-components';
+import reset from 'styled-reset'; // styled-reset이 필요한 경우
+import { useEffect, useState } from 'react';
+import LoadingScreen from './components/loading-screen';
 
 const router = createBrowserRouter([
   {
@@ -11,7 +15,7 @@ const router = createBrowserRouter([
     element: <Layout />,
     children: [
       {
-        path: '/',
+        // index: true, // 기본 라우트로 설정
         element: <Home />,
       },
       {
@@ -30,10 +34,33 @@ const router = createBrowserRouter([
   },
 ]);
 
+const GlobalStyles = createGlobalStyle`
+${reset};  // styled-reset이 필요한 경우
+*{
+  box-sizing: border-box;
+}
+body{
+  background-color: black;
+  color: white;
+  font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+}
+`;
+
 function App() {
+  const [isLoading, setIsLoading] = useState(true);
+
+  const init = async () => {
+    // wait for firebase
+    setIsLoading(false);
+  };
+
+  useEffect(() => {
+    init();
+  }, []);
   return (
     <>
-      <RouterProvider router={router} />
+      <GlobalStyles />
+      {isLoading ? <LoadingScreen /> : <RouterProvider router={router} />}
     </>
   );
 }
