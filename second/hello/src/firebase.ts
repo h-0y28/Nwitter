e16b945df6ddd5a60c3ef50ae7a646019f29570b
hyojoo <jhj080802@naver.com>
2024-09-03 20:42:20 +0900

    add descrption

--- second/hello/src/firebase.ts
+++ second/hello/src/firebase.ts
@@ -1,5 +1,6 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth/cordova';
+
 const firebaseConfig = {
   apiKey: 'AIzaSyBEPDcUshTObfy6h1o6VVLGreQaGCGQxXs',
   authDomain: 'nwitter2-f3def.firebaseapp.com',
@@ -9,6 +10,8 @@ const firebaseConfig = {
   appId: '1:961576125980:web:08f97fedd05d33755401d9',
 };
 
+// Firebase 앱 초기화
 const app = initializeApp(firebaseConfig);
 
+// Firebase 인증 인스턴스 가져오기
 export const auth = getAuth(app);
