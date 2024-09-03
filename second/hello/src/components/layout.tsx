5df16751c198406ca0ab8a4805e94f86f17a56f1
hyojoo <jhj080802@naver.com>
2024-09-03 20:41:52 +0900

    fix 주석 처리 오류

--- second/hello/src/components/layout.tsx
+++ second/hello/src/components/layout.tsx
@@ -3,8 +3,8 @@ import { Outlet } from 'react-router-dom';
 export default function Layout() {
   return (
     <>
-      <h2>kayout</h2>
-      // Outlet : 자식 라우트를 어디에 렌더링할지 결정하는 중요한 역할을 함
+      <h2>layout</h2>
+      {/* Outlet : 자식 라우트를 어디에 렌더링할지 결정하는 중요한 역할을 함 */}
       <Outlet />
     </>
   );
