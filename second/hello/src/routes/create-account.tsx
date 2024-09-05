f04af10296b4bc9ef1a834f6722d61553e648870
hyojoo <jhj080802@naver.com>
2024-09-05 16:06:06 +0900

    log in

--- second/hello/src/routes/create-account.tsx
+++ second/hello/src/routes/create-account.tsx
@@ -1,48 +1,9 @@
 import { createUserWithEmailAndPassword, updateProfile } from 'firebase/auth/cordova';
 import React, { useState } from 'react';
-import styled from 'styled-components';
 import { auth } from '../firebase';
-import { useNavigate } from 'react-router-dom';
-
-const Wrapper = styled.div`
-  height: 100%;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  width: 420px;
-  padding: 50px 0px;
-`;
-
-const Title = styled.h1`
-  font-size: 42px;
-`;
-
-const Form = styled.form`
-  margin-top: 50px;
-  display: flex;
-  flex-direction: column;
-  gap: 10px;
-  width: 100%;
-`;
-
-const Input = styled.input`
-  padding: 10px 20px;
-  border-radius: 50px;
-  border: none;
-  width: 100%;
-  font-size: 16px;
-  &[type='submit'] {
-    cursor: pointer;
-    &:hover {
-      opacity: 0.8;
-    }
-  }
-`;
-
-const Error = styled.span`
-  font-weight: 600;
-  color: tomato;
-`;
+import { Form, Link, useNavigate } from 'react-router-dom';
+import { FirebaseError } from 'firebase/app';
+import { Error, Input, Switcher, Title, Wrapper } from '../components/auth-components';
 
 export default function CreateAccount() {
   const navigate = useNavigate();
@@ -71,6 +32,7 @@ export default function CreateAccount() {
 
   const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
+    setError('');
     if (isLoading || name === '' || email === '' || password === '') return;
     try {
       setIsLoading(true);
@@ -84,6 +46,9 @@ export default function CreateAccount() {
       // home으로 이동
       navigate('/');
     } catch (e) {
+      if (e instanceof FirebaseError) {
+        setError(e.message);
+      }
     } finally {
       setIsLoading(false);
     }
@@ -99,6 +64,9 @@ export default function CreateAccount() {
         <Input type="submit" value={isLoading ? 'Loading...' : 'Create Account'} />
       </Form>
       {error !== '' ? <Error>{error}</Error> : null}
+      <Switcher>
+        Don't have an account? <Link to="/login">Login &rarr;</Link>
+      </Switcher>
     </Wrapper>
   );
 }
--- second/hello/src/routes/login.tsx
+++ second/hello/src/routes/login.tsx
@@ -1,3 +1,61 @@
+import React, { useState } from 'react';
+import { Form, Link, useNavigate } from 'react-router-dom';
+import { FirebaseError } from 'firebase/app';
+import { auth } from '../firebase';
+import { signInWithEmailAndPassword } from 'firebase/auth/cordova';
+import { Error, Input, Switcher, Title, Wrapper } from '../components/auth-components';
+
 export default function Login() {
-  return <h1>Login</h1>;
+  const navigate = useNavigate();
+
+  const [isLoading, setIsLoading] = useState(false);
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+
+  // React.ChangeEvent<HTMLInputElement> : React에서 폼 요소의 onChange 이벤트를 처리할 때 사용되는 이벤트 객체의 타입을 정의하는 TypeScript 타입
+  // <input> 요소에서 발생하는 change 이벤트를 처리할 때 사용 됨
+
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const {
+      target: { name, value },
+    } = e;
+    if (name === 'email') {
+      setEmail(value);
+    } else if (name === 'password') {
+      setPassword(value);
+    }
+  };
+
+  const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setError('');
+    if (isLoading || email === '' || password === '') return;
+    try {
+      setIsLoading(true);
+      await signInWithEmailAndPassword(auth, email, password);
+      navigate('/');
+    } catch (e) {
+      if (e instanceof FirebaseError) {
+        setError(e.message);
+      }
+    } finally {
+      setIsLoading(false);
+    }
+    console.log(email, password);
+  };
+  return (
+    <Wrapper>
+      <Title>Login 𝕏</Title>
+      <Form onSubmit={onSubmit}>
+        <Input onChange={onChange} name="email" value={email} placeholder="Email" type="email" required />
+        <Input onChange={onChange} value={password} name="password" placeholder="Password" type="password" required />
+        <Input type="submit" value={isLoading ? 'Loading...' : 'Login'} />
+      </Form>
+      {error !== '' ? <Error>{error}</Error> : null}
+      <Switcher>
+        Don't have an account? <Link to="/create-account">Create one &rarr;</Link>
+      </Switcher>
+    </Wrapper>
+  );
 }
